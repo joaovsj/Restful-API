29b29b717ca0b95e2a734d7345e9521af0bdf93b
joaovsj <joaosilva.jesus90@gmail.com>
2025-04-29 09:52:03 -0300

    :sparkles: feat: added JWT dependency and new model created

---
+++ src/main/java/com/example/restful_api/model/User.java
@@ -0,0 +1,24 @@
+package com.example.restful_api.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+}
