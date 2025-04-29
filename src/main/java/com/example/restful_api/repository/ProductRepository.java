b719ea862dea68069e94cc037303051e984266f5
joaovsj <joaosilva.jesus90@gmail.com>
2025-04-29 10:34:52 -0300

    :sparkles: feat: repository created

--- src/main/java/com/example/restful_api/repository/ProductRepository.java
+++ src/main/java/com/example/restful_api/repository/ProductRepository.java
@@ -2,6 +2,8 @@ package com.example.restful_api.repository;
 
 import com.example.restful_api.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
---
+++ src/main/java/com/example/restful_api/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.example.restful_api.repository;
+
+import com.example.restful_api.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+}
