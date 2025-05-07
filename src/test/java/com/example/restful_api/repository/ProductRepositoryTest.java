c3491d63c39c9d3e03975de79e9b38490a06b4ec
joaovsj <joaosilva.jesus90@gmail.com>
2025-05-07 11:47:14 -0300

    :test_tube: test: ProductRespositoryTest modified

--- src/test/java/com/example/restful_api/repository/ProductRepositoryTest.java
+++ src/test/java/com/example/restful_api/repository/ProductRepositoryTest.java
@@ -2,15 +2,19 @@ package com.example.restful_api.repository;
 
 import com.example.restful_api.model.Product;
 import com.example.restful_api.util.ProductCreator;
+import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
+import java.util.Optional;
 
 @DataJpaTest
+@DisplayName("Testing ProductRepository")
+@Log4j2
 class ProductRepositoryTest {
 
     @Autowired
@@ -20,11 +24,55 @@ class ProductRepositoryTest {
     @DisplayName("Method save creates Product when sucessful")
     void save_persistProductWhenSuccessful(){
 
-        Product productTobeSaved = ProductCreator.createProduct();
-        Product productSaved = productRepository.save(productTobeSaved);
+        Product productToBeSaved = ProductCreator.createProduct();
+        Product productSaved = productRepository.save(productToBeSaved);
 
         Assertions.assertNotNull(productSaved);
-        Assertions.assertEquals(productSaved.getId(), productTobeSaved.getId());
+        Assertions.assertEquals(productSaved.getId(), productToBeSaved.getId());
     }
 
+    @Test
+    @DisplayName("Find all returns list of Products when sucessful")
+    void list_returnListOfProductsWhenSuccessful(){
+        Product productToBeSaved = ProductCreator.createProduct();
+        productRepository.save(productToBeSaved);
+
+        List<Product> products = productRepository.findAll();
+
+        Assertions.assertNotNull(products);
+        Assertions.assertEquals(products.get(0).getName(), productToBeSaved.getName());
+        Assertions.assertFalse(products.isEmpty());
+    }
+
+    @Test
+    @DisplayName("Find all returns Empty list")
+    void list_returnsEmptyList(){
+        List<Product> products = productRepository.findAll();
+        Assertions.assertNotNull(products);
+        Assertions.assertTrue(products.isEmpty());
+    }
+
+    @Test
+    @DisplayName("Find by id return the correct product")
+    void findById_returnsProduct(){
+
+        Product firstProduct = new Product();
+        firstProduct.setName("Milk");
+        firstProduct.setPrice(5D);
+
+        Product secondProduct = new Product();
+        secondProduct.setName("Oil");
+        secondProduct.setPrice(10D);
+
+        Product firstProductSaved = productRepository.save(firstProduct);
+        Product secondProductSaved = productRepository.save(secondProduct);
+
+        Optional<Product> productFound = productRepository.findById(secondProductSaved.getId());
+
+        log.info(productFound);
+        log.info(secondProduct);
+
+        Assertions.assertNotNull(productFound);
+        Assertions.assertEquals(productFound.get().getName(), secondProduct.getName());
+    }
 }
