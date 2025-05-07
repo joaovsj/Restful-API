cd8aa89e69272b9adf5c1b419ffe9004aca314b5
joaovsj <joaosilva.jesus90@gmail.com>
2025-05-07 10:48:37 -0300

    :test_tube: test: ProductRepositoryTest modified

--- src/test/java/com/example/restful_api/repository/ProductRepositoryTest.java
+++ src/test/java/com/example/restful_api/repository/ProductRepositoryTest.java
@@ -1,5 +1,9 @@
 package com.example.restful_api.repository;
 
+import com.example.restful_api.model.Product;
+import com.example.restful_api.util.ProductCreator;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -13,7 +17,14 @@ class ProductRepositoryTest {
     private ProductRepository productRepository;
 
     @Test
+    @DisplayName("Method save creates Product when sucessful")
     void save_persistProductWhenSuccessful(){
 
+        Product productTobeSaved = ProductCreator.createProduct();
+        Product productSaved = productRepository.save(productTobeSaved);
+
+        Assertions.assertNotNull(productSaved);
+        Assertions.assertEquals(productSaved.getId(), productTobeSaved.getId());
     }
+
 }
--- src/test/java/com/example/restful_api/util/ProductCreator.java
+++ src/test/java/com/example/restful_api/util/ProductCreator.java
@@ -1,5 +1,13 @@
 package com.example.restful_api.util;
 
+import com.example.restful_api.model.Product;
+
 public class ProductCreator {
 
+    public static Product createProduct(){
+        Product product = new Product();
+        product.setName("Coffee");
+        product.setPrice(25D);
+        return product;
+    }
 }
